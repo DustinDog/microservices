06cd440d7200ff172e3054f9b2ec253cf816d36e
vasyl <vasyl.ihnatolia@pettersonapps.com>
2024-04-24 17:11:42 +0300

    jwt authentication

--- account_service/accounts/models.py
+++ account_service/accounts/models.py
@@ -1,12 +1,13 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 class User(AbstractUser):
     username = models.CharField(max_length=60, unique=True)
     first_name = models.CharField(max_length=30, blank=True, null=True)
     last_name = models.CharField(max_length=30, blank=True, null=True)
     email = models.EmailField(max_length=60, blank=True, null=True)
-    
+
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
 
